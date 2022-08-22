69988342624e284eaa0edc8f5b928f2c848e8639
Jonathan Noyola <wsrchoko@gmail.com>
2022-08-22 15:46:51 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+//
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /**
+       * Strip away all none-object existing properties
+       */
+      whitelist: true,
+      /***
+       * Transform input objects to their corresponding DTO objects
+       */
+      transform: true
+    })
+  );
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Hubspot')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
